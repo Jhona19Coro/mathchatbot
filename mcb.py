c82a97b0b7252d2e3bee00cc6df56d6d2119cb1e
Adrian Manea <adrianmanea@fastmail.fm>
2018-12-26 16:31:53 +0200

    read, learn, study, exercise. NOT DEFINE

--- mcb.py
+++ mcb.py
@@ -4,7 +4,19 @@
 # Author: Adrian Manea.
 ###############################################################################
 import time                      # for keeping progress
+import nltk
 from nltk.corpus import wordnet as wn
+from nltk.tag.stanford import StanfordPOSTagger
+
+
+# Setup POS Tagger
+cale_model = "/home/t3rtius/Documents/cs/sla-master/sem1/1-nlp-opt/" + \
+    "stanford-pos-tagger/stanford-postagger-full-2018-10-16/" + \
+    "models/english-bidirectional-distsim.tagger"
+cale_jar_tagger = "/home/t3rtius/Documents/cs/sla-master/sem1/1-nlp-opt/" + \
+    "stanford-pos-tagger/stanford-postagger-full-2018-10-16/" + \
+    "stanford-postagger.jar"
+tagger = StanfordPOSTagger(cale_model, cale_jar_tagger)
 
 # log file
 mcb = open("mcb.txt", "a+")      # append for progress keeping
@@ -33,21 +45,7 @@ practice = 'practice'
 read = 'read'
 exercise = 'exercise'
 mverbs = [study, learn, practice, read, exercise]
-
-# WordNet resources
-# for mw in mwords:
-#     ss = wn.synsets(mw)
-#     count = 0
-#     for s in ss:
-#         mcb.write("WORD: " + str(s.lemmas()[0].name()))
-#         mcb.write("\n")
-#         mcb.write("Sense no. " + str(count) + ": " + str(s.definition()))
-#         mcb.write("\n")
-#         print("WORD: " + str(s.lemmas()[0].name()))
-#         print("Sense no. " + str(count) + ": " + str(s.definition()))
-#         count += 1
-#     mcb.write("--------------------------------------------------\n")
-#     print("-----------------------------------------------------------------")
+mverbsWN = [vbss for verb in mverbs for vbss in wn.synsets(verb)]
 
 # MANUALLY add the appropriate senses...
 wndb = [
@@ -78,20 +76,71 @@ would = 'Would you like to '
 idk = "Sorry, I don't know anything about "
 practice = 'Practice makes perfect, you know?'
 subj = 'What subject are you interested in today?'
+notu = "Sorry, I don't understand. Could you try something else, please?"
+he = 'Hi, how can I help you today?'
+leave = 'You can always end the conversation saying goodbye.'
 
 # Giving definitions
-print(hi + subj)
+# print(hi + subj)
+# reply = str(input())
+
+# while ('BYE' not in reply.upper() and 'NO' not in reply.upper()):
+#     if reply not in wndbNames:
+#         print(idk + reply + ". " + els)
+#     else:
+#         print("I can give you the definition of that. Here it is:")
+#         for ss in wndb:
+#             if ss.lemmas()[0].name() in reply:
+#                 print("> Definition: " + ss.definition())
+#     print(more)
+#     reply = str(input())
+
+
+# Conversation
+print(he)
 reply = str(input())
 
-while ('BYE' not in reply.upper() and 'NO' not in reply.upper()):
-    if reply not in wndbNames:
-        print(idk + reply + ". " + els)
-    else:
-        print("I can give you the definition of that. Here it is:")
-        for ss in wndb:
-            if ss.lemmas()[0].name() in reply:
-                print("> Definition: " + ss.definition())
+while ('BYE' not in reply.upper()):
+    # Parse the reply, looking for nouns and verbs
+    tokens = nltk.word_tokenize(reply)
+    parts = dict(tagger.tag(tokens))
+    verbs = [part for part in parts.keys() if "VB" in parts.get(part)]
+    nouns = [part for part in parts.keys() if "NN" in parts.get(part)]
+    if (not verbs and not nouns):
+        print(notu)
+        print(leave)
+        reply = str(input())
+    # Make list of all senses of the NOUNS in the reply
+    nounSenses = [nn for noun in nouns for nn in wn.synsets(noun)]
+    # Get the one which is most similar to what's in the DB
+    # Make it a dictionary! {synset:similarity}
+    nounSimsDict = {word.path_similarity(part): word for part in
+                    nounSenses for word in wndb}
+    # Store similarity scores separately, to get max
+    nounSimsLs = [word.path_similarity(part) for part in nounSenses
+                  for word in wndb]
+    theNounSS = nounSimsDict[max(nounSimsLs)].lemmas()[0]
+    theNounName = theNounSS.name()
+    theNounDef = nounSimsDict[max(nounSimsLs)].definition()
+    verbsUpper = [v.upper() for v in verbs]
+    if ('LEARN' in verbsUpper or 'STUDY' in verbsUpper):
+        print('So, you want to learn about ' + theNounName +
+              ' or something like that.')
+        print('There are great resources on the web you should try,')
+        print(' like Wikipedia. But you better read the course manual.')
+    if 'EXERCISE' in verbsUpper:
+        print(practice)
+        print('You should check out ProjectEuler.')
+    if 'READ' in verbsUpper:
+        print('There are great resources for reading about ' +
+              theNounName + ' and related stuff.')
+        print('You could try Wikipedia or the course manual.')
+    if 'DEFIN' in verbsUpper:
+        print('I can give you the definition of ' + theNounName + '.')
+        print('Here it is: ')
+        print('> Definition: ' + theNounDef)
     print(more)
+    print(leave)
     reply = str(input())
 
 mcb.close()
